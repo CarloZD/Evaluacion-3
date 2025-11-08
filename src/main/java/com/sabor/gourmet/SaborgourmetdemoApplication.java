127966d6ed62acf1cdabb7f4b5ef265a4dc9073b
CarloZD <carlos.ruiz.l@tecsup.edu.pe>
2025-11-08 18:47:50 -0500

    PRIMER COMMIT

---
+++ src/main/java/com/sabor/gourmet/SaborgourmetdemoApplication.java
@@ -0,0 +1,13 @@
+package com.sabor.gourmet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaborgourmetdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SaborgourmetdemoApplication.class, args);
+    }
+
+}
